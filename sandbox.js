dd711433b87f424c40ce3f283dd0c85af2202bd2
Agathius <p.akos1148@gmail.com>
2022-06-27 22:19:36 +0200

    lesson-4: arrays and object literals 2

--- sandbox.js
+++ sandbox.js
@@ -1,28 +1,42 @@
-// arrays
-var names = ['luigi', 'mario', 'yoshi'];
-names.push('toad');
-// names.push(3);
-// names[1] = 3;
-var numbers = [10, 20, 12, 15];
-numbers.push(25);
-// numbers.push('shaun');
-// numbers[0] = 'shaun';
-var mixed = ['ken', 4, 'chun-li', 8, 9];
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
-// objects
-var ninja = {
-    name: 'mario',
+"use strict";
+//Arrays:
+//Implicit string:
+let names = ['pannika', 'ferike', 'jancsika'];
+names.push('juliska'); // OK
+names[0] = 'jolánka'; //OK
+//names[1] = 3; // Error -> Array's implicit type is string
+//names = 'pannika'; Error, cannot redeclare type -> redeclared type: string, initialized value type: array 
+names = ['pannika']; //Error, cannot redeclare to different type: should be array, given a string 
+console.log(names);
+//Implicit number:
+let numbers = [1, 2, 3, 4];
+//numbers.push('jolánka'); // Error
+numbers.push(5); // Ok
+console.log(numbers);
+//Mixed Arr:
+let mixed = ['jancsika', 15, false]; // not a  tuple (we can change order of elements)
+//can push multiple types to it:
+mixed.push('jenőke');
+mixed.push(50);
+mixed.push(true);
+//mixed.push(['jenőke', 50, true]); // Error, arrays were not specified as specific type 
+console.log(mixed);
+//Objects:
+let hero = {
+    name: 'mama',
     belt: 'black',
-    age: 30
-};
-ninja.age = 40;
-ninja.name = 'ryu';
-// ninja.age = '30';
-// ninja.skills = ['fighting', 'sneaking']
-ninja = {
-    name: 'yoshi',
-    belt: 'orange',
     age: 40
 };
+console.log(hero);
+//1.cannot redeclare property types:
+//hero.age = '40'; //Error
+//2.cannot add extra properties:
+//hero.superpower = [laser-eye, sharp-smell, pro cook]; //Error
+//redeclared hero should match the initial obj's structure:
+hero = {
+    name: 'SuperMom',
+    belt: 'pink',
+    age: 30,
+    //skills: ['pro cook'] // Error
+};
+console.log(hero);
--- sandbox.ts
+++ sandbox.ts
@@ -1,37 +1,47 @@
-// arrays
-let names = ['luigi', 'mario', 'yoshi'];
+//Arrays:
+//Implicit string:
+let names = ['pannika', 'ferike', 'jancsika'];
+names.push('juliska'); // OK
+names[0] = 'jolánka'; //OK
+//names[1] = 3; // Error -> Array's implicit type is string
+//names = 'pannika'; Error, cannot redeclare type -> redeclared type: string, initialized value type: array 
+ names = ['pannika']; //Error, cannot redeclare to different type: should be array, given a string 
+console.log(names);
 
-names.push('toad');
-// names.push(3);
-// names[1] = 3;
+//Implicit number:
+let numbers = [1, 2, 3, 4];
+//numbers.push('jolánka'); // Error
+numbers.push(5); // Ok
+console.log(numbers);
 
-let numbers = [10, 20, 12, 15];
+//Mixed Arr:
+let mixed = ['jancsika', 15, false]; // not a  tuple (we can change order of elements)
+//can push multiple types to it:
+mixed.push('jenőke');
+mixed.push(50);
+mixed.push(true);
+//mixed.push(['jenőke', 50, true]); // Error, arrays were not specified as specific type 
+console.log(mixed);
 
-numbers.push(25);
-// numbers.push('shaun');
-// numbers[0] = 'shaun';
-
-let mixed = ['ken', 4, 'chun-li', 8, false]; // type: string, number, boolean
-
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
-
-// objects
-let ninja = {
-  name: 'mario',
+//Objects:
+let hero = {  //Ok
+  name: 'mama',
   belt: 'black',
-  age: 30
+  age: 40
 };
+console.log(hero);
 
-ninja.age = 40;
-ninja.name = 'ryu';
-// ninja.age = '30';
-// ninja.skills = ['fighting', 'sneaking']
+//1.cannot redeclare property types:
+//hero.age = '40'; //Error
 
-ninja = {
-  name: 'yoshi',
-  belt: 'orange',
-  age: 40,
-  // skills: ['running'],
-};
+//2.cannot add extra properties:
+//hero.superpower = [laser-eye, sharp-smell, pro cook]; //Error
+
+//redeclared hero should match the initial obj's structure:
+hero = { //Ok
+  name: 'SuperMom',
+  belt: 'pink',
+  age: 30,
+  //skills: ['pro cook'] // Error
+}
+console.log(hero);
