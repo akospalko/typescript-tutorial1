d2f040ecdee454c4b06d79a0c73f4dbc5402f49e
Agathius <p.akos1148@gmail.com>
2022-06-29 00:09:05 +0200

    lesson-6

--- sandbox.ts
+++ sandbox.ts
@@ -1,24 +1,23 @@
-let age: any = 25;
-
-age = true;
+// Type any 
+/* let age: any = 25; //Ok
 console.log(age);
-age = 'hello';
+age = true; //Ok
 console.log(age);
-age = { name: 'luigi' };
+age = '25' //Ok
 console.log(age);
+age = { //Ok
+  born: 2069
+};
 
-let mixed: any[] = [];
+//any with array
+let mixedArr: any[] = [];
+['a', 1, false].forEach(e => mixedArr.push(e));
+console.log(mixedArr);
 
-mixed.push(5);
-mixed.push('mario');
-mixed.push(false);
-console.log(mixed);
-
-let ninja: { name: any, age: any };
-
-ninja = { name: 'yoshi', age: 25 };
-console.log(ninja);
-
-ninja = { name: 25, age: 'yoshi' };
-console.log(ninja);
-
+//object literal
+let hero: { name: any, age: any}; // declaring obj with type any, without assigning them any value. 
+hero = { name: 'alajos', age: 5}; //OK
+console.log(hero);
+hero = { name: 5, age: 'brown'}; //OK -> see the problem of using any (name = number, favColor = str)
+console.log(hero); 
+*/
