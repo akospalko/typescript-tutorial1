03cc857faeb2e66df11e7707435dbe83a4ac94bd
Agathius <p.akos1148@gmail.com>
2022-06-30 20:55:19 +0200

    lesson-8: using types in functions updated

--- src/sandboxTest.ts
+++ src/sandboxTest.ts
@@ -16,7 +16,7 @@ addition(5, 10); //Ok
 //addition(5, '10'); //Error, passed string type as arg.
 
 //optional(?) parameter using union(|)
-const addition2 = (a: number, b: number, c?: number | string) => { 
+const addition2 = (a: number, b: number, c/*?*/: number | string = 10) => { //c: optional (commented out) or default value if not provided 
    console.log(a + b); //Ok
    console.log(c); //Ok, if c is passed -> either num or string if empty arg -> undefined
 };
@@ -27,5 +27,11 @@ addition2(1, 2, '3');
 const substraction = (a: number, b: number): number => { // if : number is missing -> we the return type is inferred
     return a - b;
 } 
+
+const substraction2 = (a: number, b: number): void => { // function won't return anything
+    console.log(a - b);
+} 
+console.log(substraction2);
+
 
 substraction(50, 25); //Ok
